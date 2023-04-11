3425650088a7317bfdcf33a0c17193bd21e6658e
BehradX <31148015+behradkhadem@users.noreply.github.com>
2023-04-11 14:36:08 +0330

    ☺️ Added a Unit Test for FyIcons
    Now we check that each icon inside FyIcons enum is included inside each of icon packs!

---
+++ test/icon_packs/icon_packs_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fydez_elements/fydez_elements.dart';
+
+void main() {
+  test(
+    "Check that each icon pack has every icon inside [FyIcons] enum.",
+    () {
+      const usecases = FyIcons.values;
+      final iconPacksList = iconPacks;
+      for (IconPack iconPack in iconPacksList) {
+        expect(usecases.every((e) => (iconPack.pack[e] != null)), isTrue);
+      }
+    },
+  );
+}
