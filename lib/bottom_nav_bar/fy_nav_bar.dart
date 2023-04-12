4e2f299507bd8560dd8f430956fe77f3d5f64432
BehradX <31148015+behradkhadem@users.noreply.github.com>
2023-04-12 12:44:03 +0330

    ➕ Added Some Comments and Shit

--- lib/bottom_nav_bar/fy_nav_bar.dart
+++ lib/bottom_nav_bar/fy_nav_bar.dart
@@ -5,21 +5,36 @@ import 'data/nav_bar_effect.dart';
 import 'data/nav_bar_item.dart';
 import 'data/nav_bar_style.dart';
 
-// * Abstract Factory for Bottom Navigation Bar
+/// * Abstract Factory for Bottom Navigation Bar
 class FyBottomNavBar {
   /// Creates an instance of [`FyBottomNavBar`]. Make sure to use it inside a stack
-  /// instead of putting it inside `bottomNavigationBar` filed of scaffold.
+  /// instead of putting it inside `bottomNavigationBar` field of scaffold.
   static Widget createNavBar({
+    /// Required list of nav bar items to display.
     required List<NavBarItem> items,
+
+    /// Callback function that will be invoked when an item in the nav bar is tapped.
     required void Function(int)? onTap,
+
+    /// Style of the nav bar.
     NavBarStyle style = NavBarStyle.transparent,
+
+    /// Effect to apply to the nav bar.
     NavBarEffect effect = NavBarEffect.shadow,
+
+    /// Whether to display a title above the nav bar.
     bool hasTitle = false,
+
+    /// Corner radius of the nav bar.
     double cornerRadius = 0,
+
+    /// Index of the currently selected item in the nav bar.
     int currentIndex = 0,
   }) {
+    /// Check that there are at least 2 items in the nav bar.
     assert(items.length > 2, 'You should have more than 2 items.');
-    
+
+    /// Create a new instance of the nav bar using the provided parameters.
     return NavBarFactory(
       effect: effect,
     ).createNavBar(
