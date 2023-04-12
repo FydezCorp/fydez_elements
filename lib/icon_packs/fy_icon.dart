50722672798c2bfbad50eef80839c79e34b68be0
BehradX <31148015+behradkhadem@users.noreply.github.com>
2023-04-12 12:47:54 +0330

    ➕ Added Some Comments and Shit

--- lib/icon_packs/fy_icon.dart
+++ lib/icon_packs/fy_icon.dart
@@ -6,22 +6,32 @@ import 'icon_packs/iconsax_icons.dart';
 import 'icon_packs/material_icons.dart';
 
 class FyIcon implements FyIcons {
+  /// The build context used to access the fyParameters object.
   final BuildContext context;
+
+  /// The icon pack to use, if provided. If `null`, it will read the icon pack
+  /// from fyParameters object.
   final IconPacks? iconPack;
 
   FyIcon(this.context, {this.iconPack});
 
+  /// Returns the appropriate icon pack based on the current context and the provided icon pack.
   FyIcons get getIconPack {
+    // Use the provided icon pack, or the default from fyParameters if none is provided.
     final iconPack = this.iconPack ?? context.fyParameters.iconPack;
     switch (iconPack) {
+
+      // Return the Material Icons icon pack.
       case IconPacks.material:
         return MaterialIcons();
 
+      // Return the Iconsax Icons icon pack.
       case IconPacks.iconsax:
         return IconsaxIcons();
     }
   }
 
+  // Getter methods for the different icons supported by this class.
   @override
   IconData get home => getIconPack.home;
 
