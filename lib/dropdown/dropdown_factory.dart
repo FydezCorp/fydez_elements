16e2c87a1d05bfe0233697f9066803033ba8452c
BehradX <31148015+behradkhadem@users.noreply.github.com>
2023-04-17 11:46:26 +0330

    ➕ Added Disconnected Dropdown!

--- lib/dropdown/dropdown_factory.dart
+++ lib/dropdown/dropdown_factory.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fydez_elements/fydez_elements.dart';
 
 import 'connected/connected_dropdown.dart';
+import 'disconnected/disconnected_dropdown.dart';
 
 abstract class DropdownFactory<T> {
   Widget createDropDown(
@@ -21,19 +22,6 @@ abstract class DropdownFactory<T> {
       case DropdownType.modal:
         return ModalDropdown<T>();
     }
-  }
-}
-
-class DisconnectedDropdown<T> implements DropdownFactory<T> {
-  @override
-  Widget createDropDown(
-    BuildContext context, {
-    required value,
-    required List<FyDropdownItem> items,
-    required Function(T? value) onChange,
-    required double cornerRadius,
-  }) {
-    return Container();
   }
 }
 
