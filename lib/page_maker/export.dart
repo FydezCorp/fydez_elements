1a4c85d11bb8cdfbf99b785e07407537ff157246
BehradX <31148015+behradkhadem@users.noreply.github.com>
2023-05-13 15:10:48 +0330

    😅

--- lib/page_maker/export.dart
+++ lib/page_maker/export.dart
@@ -1,8 +1,3 @@
-export './pages/normal_fy_page.dart';
-export './pages/single_element_fy_page.dart';
-export 'serializer/element_serializer.dart';
 export 'fy_element.dart';
 export 'fy_page.dart';
 export 'fy_plugin.dart';
-export 'page_maker/splash/splash_fy_page.dart';
-export 'serializer/page_serializer.dart';
