aa66b83c596be743dc7f998f8aaba36606cbd32b
BehradX <31148015+behradkhadem@users.noreply.github.com>
2023-05-14 11:15:19 +0330

    🛠️ Moved Variables a Bit and Made Code Tidier

---
+++ lib/product_item/b/fy_product_item_b.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../fydez_elements.dart';
+import '../data/product_card_type.dart';
+import '../product_item_factory.dart';
+import 'product_card_b.dart';
+
+class FyProductItemB implements ProductItemFactory {
+  @override
+  Widget build(BuildContext context,
+      {required BaseProduct product,
+      required Function(BaseProduct product)? onProductTapped,
+      required double cornerRadius,
+      required ProductImageBackgroundType imageBackgroundType,
+      required BadgeLocation badgeLocation,
+      required Widget? action,
+      required Function(BaseProduct product)? onActionTapped,
+      required ProductCardType type}) {
+    return ProductCardB(
+      product: product,
+      cornerRadius: cornerRadius,
+      imageBackgroundType: imageBackgroundType,
+      badgeLocation: badgeLocation,
+      type: type,
+      action: action,
+      onActionTapped: onActionTapped,
+      onProductTapped: onProductTapped,
+    );
+  }
+}
---
+++ lib/product_item/product_item_factory.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+
+import '../fydez_elements.dart';
+import 'a/fy_product_item_a.dart';
+import 'b/fy_product_item_b.dart';
+import 'data/product_card_type.dart';
+
+abstract class ProductItemFactory {
+  Widget build(
+    BuildContext context, {
+    required BaseProduct product,
+    required Function(BaseProduct product)? onProductTapped,
+    required double cornerRadius,
+    required ProductImageBackgroundType imageBackgroundType,
+    required BadgeLocation badgeLocation,
+    required Widget? action,
+    required Function(BaseProduct product)? onActionTapped,
+    required ProductCardType type,
+  });
+  factory ProductItemFactory({required ProductCardType type}) {
+    switch (type) {
+      case ProductCardType.A:
+        return FyProductItemA();
+      case ProductCardType.B:
+        return FyProductItemB();
+    }
+  }
+}
