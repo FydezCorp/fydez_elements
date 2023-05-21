663a052d8e4c58709ec37014f0355415f5fe43d9
BehradX <31148015+behradkhadem@users.noreply.github.com>
2023-05-21 13:40:11 +0330

    🛠️ Added Skeleton to Tabbar

--- lib/skeleton/fy_skeletons.dart
+++ lib/skeleton/fy_skeletons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fydez_elements/skeleton/widgets/tabbar_skeleton.dart';
 
 import 'widgets/cart_item_skeleton.dart';
 import 'widgets/category_item_skeleton.dart';
@@ -10,5 +11,9 @@ class FySkeletons {
 
   static Widget cartItemSkeleton() {
     return const CartItemSkeleton();
+  }
+
+  static Widget tabbarSkeleton() {
+    return const TabbarSkeleton();
   }
 }
