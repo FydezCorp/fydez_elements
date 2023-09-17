8a219b11c2949a626843b338509393a925ac6fc6
BehradX <31148015+behradkhadem@users.noreply.github.com>
2023-09-17 16:49:05 +0330

    ➕ Added Feather Icon Pack!

---
+++ lib/icon_packs/icon_packs/feather_icons.dart
@@ -0,0 +1,174 @@
+import 'package:feather_icons/feather_icons.dart' as fi;
+import 'package:flutter/material.dart';
+
+import '../icon_list.dart';
+
+class FeatherIcons extends IconList {
+  @override
+  IconData get eye => fi.FeatherIcons.eye;
+
+  @override
+  IconData get heart => fi.FeatherIcons.heart;
+
+  @override
+  IconData get home => fi.FeatherIcons.home;
+
+  @override
+  IconData get login => fi.FeatherIcons.logIn;
+
+  @override
+  IconData get logout => fi.FeatherIcons.logOut;
+
+  @override
+  IconData get store => fi.FeatherIcons.shoppingBag;
+
+  @override
+  IconData get user => fi.FeatherIcons.user;
+
+  @override
+  IconData get arrowDown => fi.FeatherIcons.arrowDown;
+
+  @override
+  IconData get arrowUp => fi.FeatherIcons.arrowUp;
+
+  @override
+  IconData get search => fi.FeatherIcons.search;
+
+  @override
+  IconData get eyeSlash => fi.FeatherIcons.eyeOff;
+
+  @override
+  IconData get addToCart => fi.FeatherIcons.shoppingCart;
+
+  @override
+  IconData get appBarBack => fi.FeatherIcons.chevronsLeft;
+
+  @override
+  IconData get dropdownExpand => fi.FeatherIcons.chevronDown;
+
+  @override
+  IconData get dropdownShrink => fi.FeatherIcons.chevronUp;
+
+  @override
+  IconData get expansionTileExpand => fi.FeatherIcons.chevronDown;
+
+  @override
+  IconData get expansionTileShrink => fi.FeatherIcons.chevronUp;
+
+  @override
+  IconData get hidePassword => fi.FeatherIcons.eyeOff;
+
+  @override
+  IconData get listTileSeeMore => fi.FeatherIcons.chevronRight;
+
+  @override
+  IconData get navBarCart => fi.FeatherIcons.shoppingCart;
+
+  @override
+  IconData get navBarCartActive => fi.FeatherIcons.shoppingCart;
+
+  @override
+  IconData get navBarFavorites => fi.FeatherIcons.heart;
+
+  @override
+  IconData get navBarFavoritesActive => fi.FeatherIcons.heart;
+
+  @override
+  IconData get navBarHome => fi.FeatherIcons.home;
+
+  @override
+  IconData get navBarHomeActive => fi.FeatherIcons.home;
+
+  @override
+  IconData get navBarProfile => fi.FeatherIcons.user;
+
+  @override
+  IconData get navBarProfileActive => fi.FeatherIcons.user;
+
+  @override
+  IconData get navBarStore => fi.FeatherIcons.shoppingBag;
+
+  @override
+  IconData get navBarStoreActive => fi.FeatherIcons.shoppingBag;
+
+  @override
+  IconData get popUpClose => fi.FeatherIcons.xCircle;
+
+  @override
+  IconData get profileAccountSettings => fi.FeatherIcons.user;
+
+  @override
+  IconData get profileAddresses => fi.FeatherIcons.mapPin;
+
+  @override
+  IconData get profileContact => fi.FeatherIcons.headphones;
+
+  @override
+  IconData get profileHelp => fi.FeatherIcons.helpCircle;
+
+  @override
+  IconData get profileLogOut => fi.FeatherIcons.logOut;
+
+  @override
+  IconData get profileNotifications => fi.FeatherIcons.bell;
+
+  @override
+  IconData get profileOrderDetails => fi.FeatherIcons.shoppingBag;
+
+  @override
+  IconData get profilePassword => fi.FeatherIcons.lock;
+
+  @override
+  IconData get showPassword => fi.FeatherIcons.eye;
+
+  @override
+  IconData get changePassword => fi.FeatherIcons.key;
+
+  @override
+  IconData get email => fi.FeatherIcons.mail;
+
+  @override
+  IconData get horizontalMore => fi.FeatherIcons.moreHorizontal;
+
+  @override
+  IconData get location => fi.FeatherIcons.mapPin;
+
+  @override
+  IconData get myLocation => fi.FeatherIcons.navigation;
+
+  @override
+  IconData get phone => fi.FeatherIcons.phone;
+
+  @override
+  IconData get sms => fi.FeatherIcons.messageSquare;
+
+  @override
+  IconData get zoomIn => fi.FeatherIcons.plus;
+
+  @override
+  IconData get zoomOut => fi.FeatherIcons.minus;
+
+  @override
+  IconData get helpDelivery => fi.FeatherIcons.truck;
+
+  @override
+  IconData get helpFAQ => fi.FeatherIcons.helpCircle;
+
+  @override
+  IconData get helpGift => fi.FeatherIcons.gift;
+
+  @override
+  IconData get helpPrivacy => fi.FeatherIcons.fileText;
+
+  @override
+  IconData get helpReturn => fi.FeatherIcons.box;
+
+  @override
+  IconData get cartAdd => fi.FeatherIcons.plusCircle;
+
+  @override
+  IconData get cartRemove => fi.FeatherIcons.minusCircle;
+
+  @override
+  IconData get cartRemoveSingular => fi.FeatherIcons.trash2;
+}
